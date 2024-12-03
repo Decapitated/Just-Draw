c4e42ef09e20b78c6f288b4f0aa81b143a3e5430
Brett Johnson <brettzkyjo10@gmail.com>
2024-12-03 16:19:57 -0600

    TouchTracker Autoload

--- app/draw_cam.gd
+++ app/draw_cam.gd
@@ -1,12 +1,11 @@
 extends Camera2D
 
-@export var zoom_speed := 0.05
+@export var zoom_speed: float = 1.0
 
 var panning := false
 
 func _ready():
 	position = get_viewport().size / 2.0
-
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
@@ -15,10 +14,11 @@ func _unhandled_input(event):
 			panning = event.pressed
 		elif event.button_index == MOUSE_BUTTON_WHEEL_UP || event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			var zoom_dir = 1.0 if event.button_index == MOUSE_BUTTON_WHEEL_UP else -1.0
-			zoom = Vector2.ONE * max(zoom.x + zoom_speed * zoom_dir, 0.01)
+			zoom = Vector2.ONE * max(zoom.x + zoom_dir * zoom_speed, 0.01)
 	elif event is InputEventMouseMotion:
 		if panning:
 			position -= event.relative * (1.0 / zoom.x)
-	elif event is InputEventScreenDrag:
-		print(event.index)
-		position -= event.screen_relative * (1.0 / zoom.x)
+	elif event is InputEventPanGesture:
+		position -= event.delta * (1.0 / zoom.x)
+	elif event is InputEventMagnifyGesture:
+		zoom = Vector2.ONE * max(zoom.x + event.factor * 0.01 * zoom_speed, 0.01)
--- app/touch_tracker.gd
+++ app/touch_tracker.gd
@@ -1,58 +1,63 @@
-extends Node2D
+extends Node
 
 var touches: Dictionary
 
-var prev_distance := 0.0
-var prev_position := Vector2()
-
-var zoom_delta := 0.0
-var pan_delta := Vector2()
-
-var pos = Vector2()
-var radius = 100.0
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var prev_position: Vector2
+var prev_distance: float
 	
 func _unhandled_input(event: InputEvent) -> void:
+	# InputEventScreenTouch ~= InputEventMouseButton
+	# Only triggers on press down/up events.
 	if event is InputEventScreenTouch:
 		if event.pressed:
+			# Only allow 2 touch gestures.
 			if touches.size() < 2:
 				touches[event.index] = event
 				if touches.size() == 2:
 					prev_distance = get_gesture_distance()
 					prev_position = get_gesture_position()
+		# Remove touches as they are released.
 		elif touches.has(event.index):
 			touches.erase(event.index)
+	# InputEventScreenDrag ~= InputEventMouseMotion
+	# Only triggers on movement events.
 	elif event is InputEventScreenDrag:
+		# Update touch event data if it exists already.
 		if touches.has(event.index):
 			touches[event.index] = event
+		# Check if only two touches is available.
 		if touches.size() == 2.0:
-			var current_distance = get_gesture_distance()
-			var current_position = get_gesture_position()
-			zoom_delta = current_distance - prev_distance
-			prev_distance = current_distance
-			pan_delta = current_position - prev_position
-			prev_position = current_position
-			
-			pos += pan_delta * 10.0
-			radius += zoom_delta
-	queue_redraw()
+			# Create pan gesture if changed.
+			var current_position := get_gesture_position()
+			if current_position != prev_position:
+				create_pan_event(current_position)
+			# Create zoom gesture if changed.
+			var current_distance := get_gesture_distance()
+			if current_distance != prev_distance:
+				create_magnify_event(current_position, current_distance)
 
-func _draw() -> void:
-	draw_circle(pos, radius, Color.RED)
+# Used for pan.
+func get_gesture_position() -> Vector2:
+	var values = touches.values()
+	return ((values[0].position + values[1].position) as Vector2) / 2.0
 
 # Used for zoom.
 func get_gesture_distance() -> float:
 	var values = touches.values()
 	return ((values[0].position - values[1].position) as Vector2).length()
 
-# Used for pan.
-func get_gesture_position() -> Vector2:
-	var values = touches.values()
-	return ((values[0].position - values[1].position) as Vector2) / 2.0
+func create_pan_event(current_position: Vector2):
+	var pan_delta := current_position - prev_position
+	prev_position = current_position
+	var pan_event = InputEventPanGesture.new()
+	pan_event.position = current_position
+	pan_event.delta = pan_delta
+	Input.parse_input_event(pan_event)
+
+func create_magnify_event(current_position: Vector2, current_distance: float):
+	var zoom_delta := current_distance - prev_distance
+	prev_distance = current_distance
+	var magnify_event = InputEventMagnifyGesture.new()
+	magnify_event.position = current_position
+	magnify_event.factor = zoom_delta
+	Input.parse_input_event(magnify_event)
