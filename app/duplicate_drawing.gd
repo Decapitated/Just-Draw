26e736ee4e61f8384159894137808709a00fa2ea
Decapitated <brettzkyjo10@gmail.com>
2024-12-04 20:05:38 -0600

    Canvas Manager

--- app/duplicate_drawing.gd
+++ app/duplicate_drawing.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var drawing: CanvasLayer
+@export var canvas_manager: CanvasManager
 
 var current_canvas: CanvasData
 
@@ -20,34 +20,9 @@ func _draw():
 					draw_circle(line[0], pen.width / 2.0, pen.color)
 
 func duplicate_drawing():
-	var background = drawing.get_node("Background")
-	if background:
-		var layer_data_array: Array[LayerData] = []
-		var layers = background.find_children("*", "DrawLayer", false, false)
-		for layer in layers:
-			layer_data_array.append(layer.get_layer_data())
-
-		var canvas = CanvasData.new()
-		canvas.size = background.size
-		canvas.layers = layer_data_array
-
-		current_canvas = canvas
+		current_canvas = canvas_manager.create_canvas_data()
 		queue_redraw()
 
 func load_drawing():
 	if current_canvas != null:
-		var background = drawing.get_node("Background")
-		if background:
-			var layers = background.find_children("*", "DrawLayer", false, false)
-			for layer in layers:
-				layer.free()	
-			
-			var is_first = true
-			for layer_data in current_canvas.layers:
-				var draw_layer = DrawLayer.new()
-				background.add_child(draw_layer)
-				draw_layer.set_anchors_preset(PRESET_FULL_RECT)
-				if is_first:
-					is_first = false
-					draw_layer.active = true
-				draw_layer.load_layer_data(layer_data)
+		canvas_manager.load_canvas_data(current_canvas)
