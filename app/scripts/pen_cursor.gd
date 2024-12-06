acc55a485848060c1dd6ec67d1a6974e10977d9f
Decapitated <brettzkyjo10@gmail.com>
2024-12-06 14:23:51 -0600

    Show eraser if stylus is inverted

--- app/scripts/pen_cursor.gd
+++ app/scripts/pen_cursor.gd
@@ -2,6 +2,8 @@ extends Control
 
 @export var draw_canvas: DrawCanvas
 
+var is_eraser = false
+
 func _ready():
     mouse_entered.connect(_mouse_entered_canvas)
     mouse_exited.connect(_mouse_exited_canvas)
@@ -10,17 +12,21 @@ func _process(_delta):
     if draw_canvas:
         queue_redraw()
 
+func _unhandled_input(event):
+    if event is InputEventMouseMotion:
+        is_eraser = event.pen_inverted
+
 func _draw():
     if draw_canvas:
         var active_layer = draw_canvas.get_active_layer()
         # Only show eraser cursor if the active layer is erasing. Otherwise, show the draw cursor as default.
-        if active_layer and active_layer.get_pen_mode() == DrawLayer.ERASE:
-            draw_cursor(draw_canvas.eraser_size, 0.5)
+        if is_eraser or (active_layer and active_layer.get_pen_mode() == DrawLayer.ERASE):
+            draw_cursor(draw_canvas.eraser_size)
         else:
-            draw_cursor(draw_canvas.line_width / 2.0, 0.5)
+            draw_cursor(draw_canvas.line_width / 2.0)
 
-func draw_cursor(radius: float, width: float = 1.0):
-    draw_circle(get_local_mouse_position(), radius + width / 2.0, Color.DARK_GRAY, false, width)
+func draw_cursor(radius: float, width: float = -1.0):
+    draw_circle(get_local_mouse_position(), radius + max(width, 0.0) / 2.0, Color.DARK_GRAY, false, width)
 
 func _mouse_entered_canvas():
     Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
