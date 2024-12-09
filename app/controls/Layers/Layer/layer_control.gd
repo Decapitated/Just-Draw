10a3731beb9014797011d4db4631279a8123d15a
Decapitated <brettzkyjo10@gmail.com>
2024-12-08 21:40:59 -0600

    Selected layer styling

--- app/controls/Layers/Layer/layer_control.gd
+++ app/controls/Layers/Layer/layer_control.gd
@@ -4,6 +4,14 @@ signal layer_selected(layer: DrawLayer)
 
 var layer: DrawLayer
 
+var active_flat_panel: StyleBoxFlat = StyleBoxFlat.new()
+
+func _process(_delta):
+    if is_instance_valid(layer) && layer.active:
+        add_theme_stylebox_override("panel", active_flat_panel)
+    else:
+        remove_theme_stylebox_override("panel")
+
 func _gui_input(event):
     if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
         if event.button_index == MOUSE_BUTTON_LEFT && event.pressed && layer:
