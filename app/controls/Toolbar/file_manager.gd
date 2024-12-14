754bb7276bea1b70cf27e37305e24b75dccf506c
Decapitated <brettzkyjo10@gmail.com>
2024-12-13 22:55:46 -0600

    Save drawing on web

--- app/controls/Toolbar/file_manager.gd
+++ app/controls/Toolbar/file_manager.gd
@@ -32,8 +32,12 @@ func _on_id_pressed(id: int):
                         open_save_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
                         open_save_dialog.visible = true
             MenuOptions.SAVE:
-                open_save_dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
-                open_save_dialog.visible = true
+                match OS.get_name():
+                    "Web":
+                        save_file_web()
+                    _:
+                        open_save_dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+                        open_save_dialog.visible = true
             MenuOptions.EXPORT:
                 export_dialog.visible = true
         
@@ -55,21 +59,39 @@ func _on_export_file_selected(path: String):
 func _on_files_dropped(files: PackedStringArray):
     _on_open_save_file_selected(files[0])
 
-const TEMP_FILE = "temp_load_dhova5k2f7igh.res"
-const TEMP_FILE_PATH = "user://" + TEMP_FILE
+const TEMP_SAVE_FILE = "temp_save_dhova5k2f7igh.res"
+const TEMP_SAVE_FILE_PATH = "user://" + TEMP_SAVE_FILE
+
+func save_file_web():
+    # Check if temp file exists. If it does, delete it. ALERT: Don't know if this is needed.
+    # if FileAccess.file_exists(TEMP_SAVE_FILE_PATH):
+    #     var dir = DirAccess.open("user://")
+    #     dir.remove(TEMP_SAVE_FILE)
+    var canvas_data := draw_canvas.create_canvas_data()
+    var result := ResourceSaver.save(canvas_data, TEMP_SAVE_FILE_PATH, ResourceSaver.FLAG_COMPRESS)
+    if result == OK:
+        var buffer := FileAccess.get_file_as_bytes(TEMP_SAVE_FILE_PATH)
+        if buffer.size() > 0:
+            var uid := ResourceUID.id_to_text(ResourceUID.create_id()).trim_prefix("uid://")
+            var file_name := uid + ".res"
+            var js_window := JavaScriptBridge.get_interface("window")
+            js_window.downloadFile(Marshalls.raw_to_base64(buffer), file_name)
+
+const TEMP_LOAD_FILE = "temp_load_dhova5k2f7igh.res"
+const TEMP_LOAD_FILE_PATH = "user://" + TEMP_LOAD_FILE
 
 func _on_js_file_load(_args):
     var buffer: PackedByteArray = JavaScriptBridge.eval("getFileBuffer();", true);
     if buffer:
         # Check if temp file exists. If it does, delete it.
-        if FileAccess.file_exists(TEMP_FILE_PATH):
+        if FileAccess.file_exists(TEMP_LOAD_FILE_PATH):
             var dir = DirAccess.open("user://")
-            dir.remove(TEMP_FILE)
+            dir.remove(TEMP_LOAD_FILE)
         # Write buffer to temp file.
-        var file = FileAccess.open(TEMP_FILE_PATH, FileAccess.WRITE)
+        var file = FileAccess.open(TEMP_LOAD_FILE_PATH, FileAccess.WRITE)
         file.store_buffer(buffer)
         file.close()
         # Load temp file as CanvasData resource.
-        var res: Resource = ResourceLoader.load(TEMP_FILE_PATH, "CanvasData", ResourceLoader.CACHE_MODE_IGNORE_DEEP)
+        var res: Resource = ResourceLoader.load(TEMP_LOAD_FILE_PATH, "CanvasData", ResourceLoader.CACHE_MODE_IGNORE_DEEP)
         if res and res is CanvasData:
             draw_canvas.load_canvas_data(res)
